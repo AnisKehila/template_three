0d5c4f3c75df4c019bd53fbb42d09e2564f1b2a1
AnisKehila <aniskehila22@gmail.com>
2022-09-29 19:12:07 +0100

    added stats and discount section

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Animation on the skill bars 
 let bars = document.querySelectorAll('.skills .span');
+let skillSection = document.querySelector('.skills');
 window.addEventListener('scroll' , () => {
-    if(window.pageYOffset >= 7000) {
+    if(window.pageYOffset >= skillSection.offsetTop-300) {
         for(i = 0 ; i < bars.length ; i++) {
             bars[i].style.width = bars[i].getAttribute('data-width') ;
         }
@@ -21,3 +22,23 @@ let countDown = setInterval(() => {
     minutes.innerHTML =  Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     secondes.innerHTML = Math.floor((timeLeft % (1000 * 60)) / 1000);
 },1000);
+
+// Stats number increase 
+let started = false;
+let stats = document.querySelectorAll('.stats .number');
+window.addEventListener('scroll' , () => {
+    if(window.pageYOffset >= document.querySelector('.stats').offsetTop-300) {
+        if(!started) {
+            stats.forEach(num => {
+                let goal = num.getAttribute('data-target');
+                let count = setInterval(() => {
+                    num.textContent++ ;
+                    if(num.textContent == goal) {
+                        clearInterval(count);
+                    }
+                },1800/goal);
+            });
+            started = true;
+        }
+    }
+});
